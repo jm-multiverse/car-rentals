de2e5c6acbb9e8af234439e26e649d6cce4f7d67
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-08-06 11:13:25 -0700

    add CarsContexts.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,7 @@ import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <Container>some more texts</Container>
-      sdfsdfsd
-    </div>
+    <Container></Container>
   );
 }
 
