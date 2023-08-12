0dd934a0d245c97ea0f8b1fe4810e78423c78ad7
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-08-12 13:53:01 -0700

    adding and beginning style for car images

--- front-end/src/components/CarCard.js
+++ front-end/src/components/CarCard.js
@@ -2,12 +2,30 @@ import Card from 'react-bootstrap/Card'
 import { currencyFormatter } from '../utils'
 import { useCars } from '../contexts/CarsContext'
 
+// Random car images -
+import carImage1 from '../car-photos-thumbnails/thumbnail-1.jpg'
+import carImage2 from '../car-photos-thumbnails/thumbnail-2.jpg'
+import carImage3 from '../car-photos-thumbnails/thumbnail-3.jpg'
+import carImage4 from '../car-photos-thumbnails/thumbnail-4.jpg'
+import carImage5 from '../car-photos-thumbnails/thumbnail-5.jpg'
+import carImage6 from '../car-photos-thumbnails/thumbnail-6.jpg'
+import carImage7 from '../car-photos-thumbnails/thumbnail-7.jpg'
+import carImage8 from '../car-photos-thumbnails/thumbnail-8.jpg'
+import carImage9 from '../car-photos-thumbnails/thumbnail-9.jpg'
+import carImage10 from '../car-photos-thumbnails/thumbnail-10.jpg'
+
 export default function CarCard({
   carId,
   onViewCarClick,
+  randomImageNumber,
 }) {
   const { getCar } = useCars()
   const { make, model, manufacturingYear, color, capacity, transmission, fuelType, carType, pricePerDay, imageUrl } = getCar(carId)
+
+  // Random car image
+  const randomImages = [carImage1, carImage2, carImage3, carImage4, carImage5, carImage6, carImage7, carImage8, carImage9, carImage10,]
+
+  const carImage = randomImages[randomImageNumber]
 
   return (
     <Card className='car-card' onClick={onViewCarClick}>
@@ -16,7 +34,7 @@ export default function CarCard({
           <div className='d-flex justify-content-between'>
             <div>
               <div className='fw-bold'>{make} {model}</div>
-              <div className='text-muted'>{color}</div>
+              <img src={carImage} alt={`${make} ${model}`} className='car-card-image w-50' />
             </div>
             <div className='text-end'>
               <div className='fw-bold'>{currencyFormatter.format(pricePerDay)}</div>
